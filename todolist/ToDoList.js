31d296665971eaa501a75c7900e873e8b42d3cbf
aibalit <43064144+aibalit@users.noreply.github.com>
2020-02-05 00:35:23 +0600

    This is my To-Do List

---
+++ todolist/ToDoList.js
@@ -0,0 +1,8 @@
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
